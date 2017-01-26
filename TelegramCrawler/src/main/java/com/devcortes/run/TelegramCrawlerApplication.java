e289a286eb109d7061c8b83916bfc92ca470699f
Vitya Semenyuk <speedgear250t@gmail.com>
2017-01-26 12:32:30 +0200

    Create project

---
+++ TelegramCrawler/src/main/java/com/devcortes/run/TelegramCrawlerApplication.java
@@ -0,0 +1,12 @@
+package com.devcortes.run;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegramCrawlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelegramCrawlerApplication.class, args);
+	}
+}
